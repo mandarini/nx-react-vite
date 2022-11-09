3570e4797c378ca0a0f901f3f5f0c46def5b06f6
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-11-09 14:47:06 +0200

    import another lib

--- apps/revite/src/app/app.tsx
+++ apps/revite/src/app/app.tsx
@@ -3,6 +3,8 @@ import styles from './app.module.css';
 import NxWelcome from './nx-welcome';
 import sea from '../assets/sea.png';
 import reactLogo from '../assets/react.svg';
+import { ReviteBuildableLib } from '@nxvite/revite-buildable-lib';
+import { ReviteLib } from '@nxvite/revite-lib';
 
 export function App() {
   return (
@@ -11,6 +13,8 @@ export function App() {
       <img src={sea} className={styles.sea} alt="The sea" />
       <p>If you see the React logo, svg works.</p>
       <img src={reactLogo} className={styles.logo} alt="React logo" />
+      <ReviteBuildableLib />
+      <ReviteLib />
       <NxWelcome title="revite" />
     </div>
   );
