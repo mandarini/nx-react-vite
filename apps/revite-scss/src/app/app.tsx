4230b4c5cbfe6326cb734558a28316629dd0945c
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-11-09 14:26:46 +0200

    set up scss react app

--- apps/revite-scss/src/app/app.tsx
+++ apps/revite-scss/src/app/app.tsx
@@ -1,13 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
+import sea from '../assets/sea.png';
+import reactLogo from '../assets/react.svg';
 
 export function App() {
   return (
-    <>
+    <div className={styles['cont']}>
+      <p>If you see an image of the sea centered, assets and styles work.</p>
+      <img src={sea} className={styles['sea']} alt="The sea" />
+      <p>If you see the React logo, svg works.</p>
+      <img src={reactLogo} className={styles['logo']} alt="React logo" />
       <NxWelcome title="revite-scss" />
-      <div />
-    </>
+    </div>
   );
 }
 
