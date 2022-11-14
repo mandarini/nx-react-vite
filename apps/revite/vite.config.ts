c97bcf0732a58842cbc8bfaa51554ef699200504
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-11-14 20:19:16 +0200

    updated vite and changed structure

--- apps/revite/vite.config.ts
+++ apps/revite/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import plugin from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    plugin({
+      root: '../../',
+      projects: ['tsconfig.base.json'],
+    }),
+  ],
   server: {
     port: 3000,
   },
